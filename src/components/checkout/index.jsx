5b5c94b8f53c594f9acc58616d2f90eca3612496
LippiGian <gian.lippi@yahoo.com.ar>
2023-06-09 17:47:38 -0300

    h

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -3,57 +3,54 @@ import { useContext } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { CartContext } from '../shoppingCartContext/ShoppingCartContext';
 import {writeToFirestore} from "../createDocument/createDocument"
+import { CheckOutForm } from '../forms/checkout';
+import Swal from 'sweetalert2';
 
 const Checkout = ({totalPrice}) => {
 const [cart, setCart] = useContext(CartContext);
-  const [email, setEmail] = useState('');
-  const [direccion, setDireccion] = useState('');
-  const [nombre, setNombre] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [telefono, setTelefono] = useState('');
   const [id, setId] = useState(undefined)
 
-  const handleClose = () => {
-    setShowModal(false);
-    setId(undefined)
-  };
+  // const handleClose = () => {
+  //   setShowModal(false);
+  //   setId(undefined)
+  // };
 
-  const handleShow = () => {
-    setShowModal(true);
-  };
+  // const handleShow = () => {
+  //   setShowModal(true);
+  // };
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    // Aquí puedes hacer algo con los datos del formulario, como enviarlos a un servidor
-    console.log('Email:', email);
-    console.log('Dirección:', direccion);
-    console.log('Nombre:', nombre);
-    console.log('Teléfono:', telefono);
+  //   // Aquí puedes hacer algo con los datos del formulario, como enviarlos a un servidor
+  //   console.log('Email:', email);
+  //   console.log('Dirección:', direccion);
+  //   console.log('Nombre:', nombre);
+  //   console.log('Teléfono:', telefono);
 
-       // Reiniciar los valores de los campos del formulario
-       setEmail('');
-       setDireccion('');
-       setNombre('');
-       setTelefono('');
+  //      // Reiniciar los valores de los campos del formulario
+  //      setEmail('');
+  //      setDireccion('');
+  //      setNombre('');
+  //      setTelefono('');
 
-       //Enviar datos a firestore
-       const order = { comprador: {nombre: {nombre}, email:{email}, direccion:{direccion}, telefono: {telefono}},
-                        productos: cart,
-                      total: totalPrice}
-      //  writeToFirestore(order).then(()=>{
-      //    //Vaciar carrito, aunque faltaria validar antes de eliminar
+  //      //Enviar datos a firestore
+  //      const order = { comprador: {nombre: {nombre}, email:{email}, direccion:{direccion}, telefono: {telefono}},
+  //                       productos: cart,
+  //                     total: totalPrice}
+  //     //  writeToFirestore(order).then(()=>{
+  //     //    //Vaciar carrito, aunque faltaria validar antes de eliminar
        
-      //   setCart([]);
-      //  });
-      escribir(order)
+  //     //   setCart([]);
+  //     //  });
+  //     escribir(order)
   
       
         
       
       
-  };
+  // };
 
 
 
@@ -65,6 +62,7 @@ const escribir = async (order)=>{
   {
     setCart([]);
     setId(response.id);
+    handleButtonClick(response.id)
   }
   else
   {
@@ -97,107 +95,123 @@ const escribir = async (order)=>{
   // };
 
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+  // const handleEmailChange = (e) => {
+  //   setEmail(e.target.value);
+  // };
 
-  const handleDireccionChange = (e) => {
-    setDireccion(e.target.value);
-  };
+  // const handleDireccionChange = (e) => {
+  //   setDireccion(e.target.value);
+  // };
 
-  const handleNombreChange = (e) => {
-    setNombre(e.target.value);
-  };
+  // const handleNombreChange = (e) => {
+  //   setNombre(e.target.value);
+  // };
 
-  const handleTelefonoChange = (e) => {
-    const inputTelefono = e.target.value;
-    // Remover cualquier carácter que no sea un número
-    const soloNumeros = inputTelefono.replace(/[^0-9]/g, '');
-    setTelefono(soloNumeros);
+  // const handleTelefonoChange = (e) => {
+  //   const inputTelefono = e.target.value;
+  //   // Remover cualquier carácter que no sea un número
+  //   const soloNumeros = inputTelefono.replace(/[^0-9]/g, '');
+  //   setTelefono(soloNumeros);
 
-  };
+  // };
 
-  return (
-     <>
 
-     <Button variant="success" className='m-auto' onClick={handleShow}>
-        Finalizar compra
-      </Button>
-      {/* <Modal show={showModal2} onHide={handleClose2}>
-        <Modal.Header closeButton>
-
-        </Modal.Header>
-      </Modal> */}
-
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Información del cliente</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">Correo electrónico</label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-        <div className="form-text">Por favor, introduce un correo electrónico válido.</div>
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="direccion" className="form-label">Dirección</label>
-        <input
-          type="text"
-          className="form-control"
-          id="direccion"
-          value={direccion}
-          onChange={handleDireccionChange}
-          required
-        />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="nombre" className="form-label">Nombre completo</label>
-        <input
-          type="text"
-          className="form-control"
-          id="nombre"
-          value={nombre}
-          onChange={handleNombreChange}
-          required
-        />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="telefono" className="form-label">Teléfono</label>
-        <input
-          type="text"
-          className="form-control"
-          id="telefono"
-          value={telefono}
-          onChange={handleTelefonoChange}
-          required
-        />
-      </div>
+  console.log(id)
+  const handleButtonClick = (id) => {
+    Swal.fire({
+      title: '¡Su orden de compra ha sido creada con exito!',
+      text: 'Su numero de orden es: '+id,
+      icon: 'success',
+      confirmButtonText: 'Cerrar',
       
-      <button type="submit" className="btn btn-primary">Enviar</button>
-    </form>
-        </Modal.Body>
+    })
+  };
+
+   return (
+    <>
+    <CheckOutForm totalPrice={totalPrice} escribir={escribir} ></CheckOutForm>
+
+    </>
+  //    <>
+
+  //    <Button variant="success" className='m-auto' onClick={handleShow}>
+  //       Finalizar compra
+  //     </Button>
+  //     {/* <Modal show={showModal2} onHide={handleClose2}>
+  //       <Modal.Header closeButton>
+
+  //       </Modal.Header>
+  //     </Modal> */}
+
+  //     <Modal show={showModal} onHide={handleClose}>
+  //       <Modal.Header closeButton>
+  //         <Modal.Title>Información del cliente</Modal.Title>
+  //       </Modal.Header>
+  //       <Modal.Body>
+  //       <form onSubmit={handleSubmit}>
+  //     <div className="mb-3">
+  //       <label htmlFor="email" className="form-label">Correo electrónico</label>
+  //       <input
+  //         type="email"
+  //         className="form-control"
+  //         id="email"
+  //         value={email}
+  //         onChange={handleEmailChange}
+  //         required
+  //       />
+  //       <div className="form-text">Por favor, introduce un correo electrónico válido.</div>
+  //     </div>
+
+  //     <div className="mb-3">
+  //       <label htmlFor="direccion" className="form-label">Dirección</label>
+  //       <input
+  //         type="text"
+  //         className="form-control"
+  //         id="direccion"
+  //         value={direccion}
+  //         onChange={handleDireccionChange}
+  //         required
+  //       />
+  //     </div>
+
+  //     <div className="mb-3">
+  //       <label htmlFor="nombre" className="form-label">Nombre completo</label>
+  //       <input
+  //         type="text"
+  //         className="form-control"
+  //         id="nombre"
+  //         value={nombre}
+  //         onChange={handleNombreChange}
+  //         required
+  //       />
+  //     </div>
+
+  //     <div className="mb-3">
+  //       <label htmlFor="telefono" className="form-label">Teléfono</label>
+  //       <input
+  //         type="text"
+  //         className="form-control"
+  //         id="telefono"
+  //         value={telefono}
+  //         onChange={handleTelefonoChange}
+  //         required
+  //       />
+  //     </div>
+      
+  //     <button type="submit" className="btn btn-primary">Enviar</button>
+  //   </form>
+  //       </Modal.Body>
         
-        <Modal.Footer>
-        {id && <><h3>¡Su orden de compra ha sido creada con exito!</h3>
-                <h2>Su numero de orden es: {id}</h2></>}
+  //       <Modal.Footer>
+  //       {id && <><h3>¡Su orden de compra ha sido creada con exito!</h3>
+  //               <h2>Su numero de orden es: {id}</h2></>}
          
-        <button className='btn btn-dark' onClick={handleClose}>Cerrar</button>
-        </Modal.Footer>
-      </Modal>
+  //       <button className='btn btn-dark' onClick={handleClose}>Cerrar</button>
+  //       </Modal.Footer>
+  //     </Modal>
 
      
-    </>
+  //   </>
   );
 };
 
